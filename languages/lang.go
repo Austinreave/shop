782405e2690becd1c248fa5c3132675318f2db8a
fangyuyu <1182849075@qq.com>
2019-09-19 14:24:40 +0800

    dd

---
+++ languages/lang.go
@@ -0,0 +1,9 @@
+package languages
+
+var(
+	ValidationFails = "validation fails"
+	FileTypeError = "file type error"
+	ParameterAcquisitionFailed = "parameter acquisition failed"
+	sliceError = "slice len is 0"
+	sliceFindError = "slices find not param"
+)
