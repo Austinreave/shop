621184045f74a21975fc066ad5e1695d5296f22e
fangyuyu <1182849075@qq.com>
2019-09-23 18:57:12 +0800

    ddd

--- config/config.go
+++ config/config.go
@@ -20,4 +20,7 @@ const(
     PROJECT_SECRET = "cc7fb6e1278990edf87f3e4f0f9c3a51"
     API_HOST = "https://smlo.tsign.cn/opentreaty-service/"
     AddPersonUrl = API_HOST+"account/create/person" //创建个人账户地址
+    AddOrganizeUrl = API_HOST+"account/create/organize/common";//创建企业账户地址
+    SilentSignUrl = API_HOST+"account/platform/silentsign";//设置静默签署授权地址
+    GetUploadurl = API_HOST+"file/uploadurl";//获取文件直传地址
 )
--- handle/contract.go
+++ handle/contract.go
@@ -3,11 +3,13 @@ import (
 	"shop/utils"
 	"net/http"
 	"errors"
+	"shop/config"
+	"fmt"
 )
 
 func AccountCreatePerson(r *http.Request, param map[string]interface{}) (interface{}, error) {
 
-	data, err :=utils.HttpPostData(param)
+	data, err :=utils.HttpPostData(config.AddPersonUrl,param)
 
 	if err != nil {
 		return nil, err
@@ -21,3 +23,65 @@ func AccountCreatePerson(r *http.Request, param map[string]interface{}) (interfa
 
 	return temp, nil
 }
+
+func CreateOrganizeCommon(r *http.Request, param map[string]interface{}) (interface{}, error) {
+
+	data, err :=utils.HttpPostData(config.AddOrganizeUrl,param)
+	fmt.Println(data)
+	if err != nil {
+		return nil, err
+	}
+
+	if data["errCode"].(float64) != 0 {
+		return nil,  errors.New(data["msg"].(string))
+	}
+
+	temp := data["data"].(map[string]interface {})
+
+	return temp, nil
+}
+
+
+func SilentSign(r *http.Request, param map[string]interface{}) (interface{}, error) {
+
+	data, err :=utils.HttpPostData(config.SilentSignUrl,param)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if data["errCode"].(float64) != 0 {
+		return nil,  errors.New(data["msg"].(string))
+	}
+
+	temp := data["data"]
+
+	return temp, nil
+}
+
+func UploadTemplateFile(r *http.Request, param map[string]interface{}) (interface{}, error){
+
+	data, err :=utils.GetUploadurl(config.SilentSignUrl,param)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if data["errCode"].(float64) != 0 {
+		return nil,  errors.New(data["msg"].(string))
+	}
+
+	temp := data["data"]
+
+	return temp, nil
+
+}
+
+
+
+
+
+
+
+
+
